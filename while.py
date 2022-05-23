a8edf7918ce6cd53e1a8c70693f72d13bfc17f3e
jose-rojas-loadsure <developer.halloworld@gmail.com>
2022-05-23 11:12:13 -0500

    first commmit

---
+++ while.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Basic While
+
+"""year = 2000
+while year <= 2022:
+    print ("Informs to year", str(year))
+    year += 1"""
+
+# While name
+
+while True:
+    name = input("Insert your name: ")
+    if name:
+        break
+
