35ef6fd7cd06334b1fc64038f8e3b79372f36df1
jose-rojas-loadsure <developer.halloworld@gmail.com>
2022-05-25 13:15:45 -0500

    we add #point in 2D

--- list.py
+++ list.py
@@ -1,6 +1,6 @@
 # This is a list: It´s recognized by the symbols [,,,].
 
-run_trigger = input("Please RUN TRIGGER, tap ENTER.")
+"""run_trigger = input("Please RUN TRIGGER, tap ENTER.")
 
 list = [2, 4, 5, 7, 8, 9, 3, 4,10,14,22,21,70,98,100]
 for i in list:  # Iterator i in our list.
@@ -8,4 +8,13 @@ for i in list:  # Iterator i in our list.
         continue
     print(i)
 
-print("Thanks for follow that instrucction.")
+print("Thanks for follow that instrucction.")"""
+
+
+# Point in 2D.
+
+puntos = [(0,0), (3,5), (-1,4), (10,2)]
+for x,y in puntos:
+    print("Px = {0},  Py ={1}".format(x,y))
+
+
